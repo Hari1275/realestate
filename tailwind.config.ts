87609b068f9a3dfe23edc0c2ff74961e0fb25da2
Khari1275 <khari1275@gmail.com>
2023-10-05 21:49:49 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        '8xl': '7.5rem',
+      },
     },
   },
-  plugins: [],
-}
-export default config
+
+  plugins: [require('daisyui')],
+};
+export default config;
